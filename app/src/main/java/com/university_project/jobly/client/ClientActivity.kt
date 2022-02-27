2baa823d3b127320bc5de06789ff6b53832566c8
dipankar108 <nathbd10@gmail.com>
2022-02-27 11:31:58 +0600

    Remove unused code and adding fragment navbar

--- app/src/main/java/com/university_project/jobly/client/ClientActivity.kt
+++ app/src/main/java/com/university_project/jobly/client/ClientActivity.kt
@@ -1,31 +1,29 @@
 package com.university_project.jobly.client
-
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import com.university_project.jobly.R
 import com.university_project.jobly.databinding.ActivityClientBinding
-
 class ClientActivity : AppCompatActivity() {
-  private  lateinit var binding:ActivityClientBinding
-    @SuppressLint("ResourceType")
+    private lateinit var binding: ActivityClientBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityClientBinding.inflate(layoutInflater)
+        binding = ActivityClientBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.bmnClientNavBar.menu.findItem(R.id.client_myPost_menu_id).isChecked=true
+        binding.bmnClientNavBar.menu.findItem(R.id.client_myPost_menu_id).isChecked = true
         binding.bmnClientNavBar.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.client_myPost_menu_id->changeFragment(ClientJobPostFragment())
-
+            when (it.itemId) {
+                R.id.client_myPost_menu_id -> changeFragment(ClientJobPostFragment())
+                R.id.client_applied_menu_id -> changeFragment(ClientAppliedFragment())
+                R.id.client_callForInterview_menu_id -> changeFragment(
+                    ClientCallForInterViewFragment()
+                )
             }
-           return@setOnItemSelectedListener true
-       }
+            return@setOnItemSelectedListener true
+        }
     }
-
-    private fun changeFragment(fragment:Fragment) {
-     supportFragmentManager.beginTransaction().replace(R.id.fl_clientLayout_id,fragment).commit()
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fl_clientLayout_id, fragment)
+            .commit()
     }
 }
