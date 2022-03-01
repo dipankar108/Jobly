e6bbeb5e464fb30a3d72c5ec1dac20411de6059c
dipankar108 <nathbd10@gmail.com>
2022-03-01 12:51:26 +0600

    Connect recycler view to firebase database

--- app/src/main/java/com/university_project/jobly/client/ClientJobPostFragment.kt
+++ app/src/main/java/com/university_project/jobly/client/ClientJobPostFragment.kt
@@ -2,42 +2,42 @@ package com.university_project.jobly.client
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.university_project.jobly.R
-import com.university_project.jobly.client.clientviewmodel.ClientPostDataModel
+import com.university_project.jobly.client.adapter.PostViewAdapter
 import com.university_project.jobly.client.clientviewmodel.ClientPostViewModel
-import com.university_project.jobly.databinding.FragmentClientAppliedBinding
 import com.university_project.jobly.databinding.FragmentClientJobPostBinding
-import com.university_project.jobly.datamodel.CreatePostModel
 
 class ClientJobPostFragment : Fragment() {
-    private  lateinit var _binding:FragmentClientJobPostBinding
+    private lateinit var _binding: FragmentClientJobPostBinding
     private lateinit var liveDataModel: ClientPostViewModel
-    private lateinit var auth:FirebaseAuth
-    private val binding get()=_binding
+    private lateinit var auth: FirebaseAuth
+    private val binding get() = _binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding=FragmentClientJobPostBinding.inflate(layoutInflater,container,false)
+        _binding = FragmentClientJobPostBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        auth=Firebase.auth
-        liveDataModel= ViewModelProvider(this)[ClientPostViewModel::class.java]
-        liveDataModel.getLiveData().observe(viewLifecycleOwner,{
-            Log.d("mTAG", "onViewCreated: $it")
+        auth = Firebase.auth
+        val postViewAdapter = PostViewAdapter()
+        binding.rvClientPostViewId.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvClientPostViewId.adapter = postViewAdapter
+        liveDataModel = ViewModelProvider(this)[ClientPostViewModel::class.java]
+        liveDataModel.getLiveData().observe(viewLifecycleOwner, {
+            postViewAdapter.setArrayList(it)
+            postViewAdapter.notifyDataSetChanged()
         })
     }
 }
---
+++ app/src/main/java/com/university_project/jobly/client/adapter/PostViewAdapter.kt
@@ -0,0 +1,41 @@
+package com.university_project.jobly.client.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.university_project.jobly.R
+import com.university_project.jobly.client.clientviewmodel.ClientPostDataModel
+
+class PostViewAdapter : RecyclerView.Adapter<PostViewAdapter.PostViewHolder>() {
+    private val myArrayList = arrayListOf<ClientPostDataModel>()
+    class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val postTitle: TextView = itemView.findViewById(R.id.tv_postViewTitle_id)
+        val postDesc: TextView = itemView.findViewById(R.id.tv_postViewDesc_id)
+        val position: TextView = itemView.findViewById(R.id.tv_postViewPosition_id)
+        val experience: TextView = itemView.findViewById(R.id.tv_postViewExperiment_id)
+        val location: TextView = itemView.findViewById(R.id.tv_postViewLocation_id)
+    }
+
+    fun setArrayList(arrayList: ArrayList<ClientPostDataModel>) {
+        myArrayList.addAll(arrayList)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.postitemview, parent, false)
+        return PostViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val res = myArrayList[position]
+        holder.postTitle.text = res.title
+        holder.postDesc.text = res.desc
+        holder.position.text = res.category
+        holder.experience.text = res.experience.toString()
+        holder.location.text = res.location
+    }
+    override fun getItemCount(): Int {
+        return myArrayList.size
+    }
+}
