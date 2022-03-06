8a200e435b93cb1926527360c8fa0f90d47f8884
dipankar108 <nathbd10@gmail.com>
2022-03-06 15:25:49 +0600

    applied employee list activity added to the apps

---
+++ app/src/main/java/com/university_project/jobly/AppliedEmployeeActivity.kt
@@ -0,0 +1,11 @@
+package com.university_project.jobly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppliedEmployeeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_applied_employee)
+    }
+}
