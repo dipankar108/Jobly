f04286fcce8dc98f4ae308815d02e1842e5a8138
dipankar108 <nathbd10@gmail.com>
2022-03-22 13:25:00 +0600

    offline when signout

--- app/src/main/java/com/university_project/jobly/utils/UtilClass.kt
+++ app/src/main/java/com/university_project/jobly/utils/UtilClass.kt
@@ -7,13 +7,15 @@ import android.content.SharedPreferences
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.university_project.jobly.accountlog.AccountLog
+import com.university_project.jobly.baseviewmodel.Repository
 
 object UtilClass {
-    fun signOutNow(context: Context, activity: Activity, sh: SharedPreferences.Editor,) {
+    fun signOutNow(context: Context, activity: Activity, sh: SharedPreferences.Editor) {
+        Repository.updateActiveStatus(false)
         Firebase.auth.signOut()
         sh.clear().apply()
         val intent = Intent(context, AccountLog::class.java)
-        intent.flags=Intent.FLAG_ACTIVITY_CLEAR_TASK
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         context.startActivity(intent)
         activity.finish()
