b78fb685281e17f611436aeb5ea363da6c2a912a
dipankar108 <nathbd10@gmail.com>
2022-04-02 12:20:48 +0600

    new design added

--- app/src/main/java/com/university_project/jobly/utils/AppLifeCycleListner.kt
+++ app/src/main/java/com/university_project/jobly/utils/AppLifeCycleListner.kt
@@ -1,18 +1,17 @@
 package com.university_project.jobly.utils
 
-import android.util.Log
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import com.university_project.jobly.baseviewmodel.Repository
 
-class AppLifeCycleListner:DefaultLifecycleObserver {
+class AppLifeCycleListner : DefaultLifecycleObserver {
     override fun onPause(owner: LifecycleOwner) {
         super.onPause(owner)
-       Repository.updateActiveStatus(false)
+        Repository.updateActiveStatus(false)
     }
 
     override fun onStart(owner: LifecycleOwner) {
         super.onStart(owner)
-       Repository.updateActiveStatus(true)
+        Repository.updateActiveStatus(true)
     }
 }
